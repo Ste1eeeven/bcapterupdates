2de189361eab6cf3118f0b05009a478be92c91b5
Ste1eeeven <85071398+Ste1eeeven@users.noreply.github.com>
2021-05-31 01:20:03 +0600

    Add files via upload

--- bcapterupdate.lua
+++ bcapterupdate.lua
@@ -1,50 +1,64 @@
-require 'lib.moonloader'
+script_name('Autoupdate script') -- пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ
+script_author('FORMYS') -- пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ
+script_description('Autoupdate') -- пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ
 
-local dlstatus = require ('moonloader').download_status
-local sv = 2
-local sv_t = '1.50'
-local script_path = thisScript().path
-local script_url = 'https://raw.githubusercontent.com/Ste1eeeven/bcapterupdates/main/bcapterupdate.lua'
+require "lib.moonloader" -- пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
+local dlstatus = require('moonloader').download_status
 local inicfg = require 'inicfg'
-local update_path = getWorkingDirectory()..'/update.ini'
-local update_url = 'https://raw.githubusercontent.com/Ste1eeeven/bcapterupdates/main/update.ini'
- 
+local keys = require "vkeys"
+local imgui = require 'imgui'
+local encoding = require 'encoding'
+encoding.default = 'CP1251'
+u8 = encoding.UTF8
+
 update_state = false
 
-function main()
-	repeat wait(0) until isSampAvailable()
-	while not isSampAvailable() do wait(0) end
-	_, id = sampGetPlayerIdByCharHandle(PLAYER_PED)
-	nick_name = sampGetPlayerNickname(id)
+local script_vers = 1
+local script_vers_text = "1.00"
 
-	sampRegisterChatCommand('update', cmd_update)
-	sampAddChatMessage('loaded', -1)
-	
-	downloadUrlToFile(update_url, update_path,function(id, status)
-		if status == dlstatus.STATUS_ENDDOWNLOADDATA then
-			updateini =  inicfg.load(nil, update_path)
-			if tonumber(updateini.info.vers) > sv then
-				sampAddChatMessage('Update is: true. Version: '.. updateini.info.vers_text, -1)
-				update_state = true
-			end
-		end
-	end)
+local update_url = "https://raw.githubusercontent.com/Ste1eeeven/bcapterupdates/main/bcapterupdate.lua" -- пїЅпїЅпїЅ пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ
+local update_path = getWorkingDirectory() .. "/update.ini" -- пїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ
+
+local script_url = "https://raw.githubusercontent.com/Ste1eeeven/bcapterupdates/main/update.ini" -- пїЅпїЅпїЅ пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ
+local script_path = thisScript().path
+
+
+function main()
+	if not isSampLoaded() or not isSampfuncsLoaded() then return end
+    while not isSampAvailable() do wait(100) end
+    
+    sampRegisterChatCommand("update", cmd_update)
+
+	_, id = sampGetPlayerIdByCharHandle(PLAYER_PED)
+    nick = sampGetPlayerNickname(id)
+
+    downloadUrlToFile(update_url, update_path, function(id, status)
+        if status == dlstatus.STATUS_ENDDOWNLOADDATA then
+            updateIni = inicfg.load(nil, update_path)
+            if tonumber(updateIni.info.vers) > script_vers then
+                sampAddChatMessage("пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ! пїЅпїЅпїЅпїЅпїЅпїЅ: " .. updateIni.info.vers_text, -1)
+                update_state = true
+            end
+            os.remove(update_path)
+        end
+    end)
+    
 	while true do
-		wait(0)
-		
-		if update_state then
-			downloadUrlToFile(script_url, script_path,function(id, status)
-				if status == dlstatus.STATUS_ENDDOWNLOADDATA then
-					sampAddChatMessage("Update loaded: true", -1)
-					thisScript():reload()
-				end
-			end)
-			break
-		end
-	
+        wait(0)
+
+        if update_state then
+            downloadUrlToFile(script_url, script_path, function(id, status)
+                if status == dlstatus.STATUS_ENDDOWNLOADDATA then
+                    sampAddChatMessage("scriptupdate", -1)
+                    thisScript():reload()
+                end
+            end)
+            break
+        end
+
 	end
 end
 
 function cmd_update(arg)
-	sampShowDialog(2228, 'Autoupdate v1.5', 'text', 'close', '', 0)
+    sampShowDialog(1000, "autoupv2", "avtoup", "close", "", 0)
 end
